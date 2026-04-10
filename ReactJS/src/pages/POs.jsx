6a4a323268c685858b1936596d6768c5a5b80d4a
shadzx09 <lagoyoshadia6@gmail.com>
2026-04-11 00:21:45 +0800

    dasda

--- ReactJS/src/pages/POs.jsx
+++ ReactJS/src/pages/POs.jsx
@@ -984,7 +984,7 @@ const POs = () => {
 
                 <div className="for-receipt-bottom">
                   <div className="for-receipt-calendar">
-                    laundry to be claimed on <br />
+                    <span style={{ fontSize: 12, color: '#666' }}>Laundry to be claimed on:</span> <br />
                     <input type="date" className="for-receipt-date" value={dueDate} onChange={e => setDueDate(e.target.value)} />
                   </div>
                 </div>
